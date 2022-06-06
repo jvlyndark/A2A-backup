57a0cc91c8ce7827efb9dcefc873ea4758b58eed
jvlyndark <50807142+jvlyndark@users.noreply.github.com>
2022-06-06 15:32:56 -0400

    add username validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    validates :username, presence: true, 
+                        uniqueness: { case_sensitive: false }, 
+                        length: { minimum: 3, maximum: 15 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_06_184827) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_06_192503) do
   create_table "users", force: :cascade do |t|
-    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
   end
 
 end
