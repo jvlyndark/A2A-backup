71c2aa4221d49f8bf088e82071fb27f17af13a4e
jvlyndark <50807142+jvlyndark@users.noreply.github.com>
2022-06-09 12:46:59 -0400

    added secure password to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+    before_save { self.username = username.downcase }
+
     has_one :profile
     validates :username, presence: true, 
                         uniqueness: { case_sensitive: false }, 
                         length: { minimum: 3, maximum: 15 }
+    has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_06_193722) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_09_164357) do
   create_table "profiles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_06_193722) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.string "password_digest"
   end
 
 end
