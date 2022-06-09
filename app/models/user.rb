f8097e491332ff407a952f58ea851b108535382e
jvlyndark <50807142+jvlyndark@users.noreply.github.com>
2022-06-09 14:18:10 -0400

    re-creating users and sessions using devise gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 class User < ApplicationRecord
-    before_save { self.username = username.downcase }
-
-    has_one :profile
-    validates :username, presence: true, 
-                        uniqueness: { case_sensitive: false }, 
-                        length: { minimum: 3, maximum: 15 }
-    has_secure_password
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
