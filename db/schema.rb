314f928ab4805cd8270712520b567d5cdb7ff24c
jvlyndark <50807142+jvlyndark@users.noreply.github.com>
2022-06-15 15:55:05 -0400

    add admin user functionality + create admin user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_15_182020) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_15_194758) do
   create_table "profiles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_15_182020) do
     t.datetime "updated_at", null: false
     t.string "username"
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
