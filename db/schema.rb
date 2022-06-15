4756ba3fe6045b4de90fc19c61a08c9ff9027851
jvlyndark <50807142+jvlyndark@users.noreply.github.com>
2022-06-15 14:21:54 -0400

    create session model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_09_164357) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_15_182020) do
   create_table "profiles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -18,6 +18,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_09_164357) do
     t.string "name"
     t.integer "age"
     t.string "location"
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
