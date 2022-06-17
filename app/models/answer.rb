11defc68d05d16cbe91eff954c1064b2b7733e5f
jvlyndark <50807142+jvlyndark@users.noreply.github.com>
2022-06-17 14:04:23 -0400

    update answer, profile associations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,2 +1,4 @@
 class Answer < ApplicationRecord
+    belongs_to :user_answer, class_name: "user_answer", foreign_key: "user_answer_id"
+    belongs_to :match_answer, class_name: "match_answer", foreign_key: "match_answer_id"
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,4 @@
 class Profile < ApplicationRecord
-    belongs_to :user, class_name: "user", foreign_key: "user_id"
+    belongs_to :user, class_name: "User", foreign_key: "user_id"
+    has_many :answers, class_name: "Answer", foreign_key: "answer_id"
 end
